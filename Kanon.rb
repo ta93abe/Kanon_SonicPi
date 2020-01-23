ab7af12b97fed03cdddc086d26813cbf250d356b
Takumi Abe <33920723+ta93abe@users.noreply.github.com>
2020-01-23 18:56:06 +0900

    upload chord file

---
+++ Kanon.rb
@@ -0,0 +1,21 @@
+# ÉJÉmÉìêiçs
+
+live_loop :live do
+  use_synth :fm
+  play chord(:C5, :M)
+  sleep 1.5
+  play chord(:G4, :M)
+  sleep 1.5
+  play chord(:A4, :m)
+  sleep 1.5
+  play chord(:E4, :m)
+  sleep 1.5
+  play chord(:F4, :M)
+  sleep 1.5
+  play chord(:C4, :M)
+  sleep 1.5
+  play chord(:F4, :M)
+  sleep 1.5
+  play chord(:G4, :M)
+  sleep 1.5
+end
